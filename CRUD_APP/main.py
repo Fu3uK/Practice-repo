fa98971e8d9d29e1edc70ca8c7e2addc2111f747
DHot <dencw123@mail.ru>
2021-07-04 15:55:30 +0300

    Придуманы основные классы
    Придуманы основные классы и загружены необходимые библиотеки

--- CRUD_APP/main.py
+++ CRUD_APP/main.py
@@ -1,9 +1,23 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+DB_NAME = "films1.db"
 
 
+#class MyWidget(QMainWindow)
 
 
+#class AddFilmWidget(QMainWindow)
 
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 if __name__ == '__main__':
-    #Code
-
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
