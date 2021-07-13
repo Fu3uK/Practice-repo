d009962c34be7746163bc43235a596f70b2de387
DHot <dencw123@mail.ru>
2021-07-13 17:10:21 +0300

    Дописана main функция

--- CRUD_APP/main.py
+++ CRUD_APP/main.py
@@ -131,5 +131,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
